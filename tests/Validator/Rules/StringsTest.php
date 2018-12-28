54ad7d8a520402fbb8d0bce231ac278516a058b0
Jozef Grencik <20k121@gmail.com>
2018-12-28 14:28:46 +0100

    add Tests for Validator::string()->minLength()

---
+++ tests/Validator/Rules/StringsTest.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Validator\Rules;
+
+use JozefGrencik\Validator\Exceptions\InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use JozefGrencik\Validator\Validator;
+
+class StringsTest extends TestCase {
+
+    /**
+     * Tests for Validator::string()->length()
+     * @throws \Exception
+     */
+    public function testLength() {
+
+    }
+
+    /**
+     * Tests for Validator::string()->notEmpty()
+     * @throws \Exception
+     */
+    public function testNotEmpty() {
+        $validator = Validator::string()->notEmpty();
+
+        //false
+        $this->assertFalse($validator->isValid(''));
+
+        //true
+        $this->assertTrue($validator->isValid(' '));
+        $this->assertTrue($validator->isValid('a'));
+        $this->assertTrue($validator->isValid('0'));
+        $this->assertTrue($validator->isValid('č'));
+    }
+
+    /**
+     * Tests for Validator::string()->lengthBetween()
+     * @throws \Exception
+     */
+    public function testLengthBetween() {
+
+    }
+
+    /**
+     * Tests for Validator::string()->maxLength()
+     * @throws \Exception
+     */
+    public function testMaxLength() {
+
+    }
+
+    /**
+     * Tests for Validator::string()->empty()
+     * @throws \Exception
+     */
+    public function testEmpty() {
+        $validator = Validator::string()->empty();
+
+        //true
+        $this->assertTrue($validator->isValid(''));
+
+        //false
+        $this->assertFalse($validator->isValid(' '));
+        $this->assertFalse($validator->isValid('a'));
+        $this->assertFalse($validator->isValid('0'));
+        $this->assertFalse($validator->isValid('č'));
+    }
+
+    /**
+     * Tests for Validator::string()->minLength()
+     * @throws \Exception
+     */
+    public function testMinLength() {
+        $wasException = FALSE;
+        try {
+            Validator::string()->minLength(-1);
+        } catch (InvalidArgumentException $exception) {
+            $wasException = TRUE;
+        }
+        $this->assertTrue($wasException);
+
+
+        $validator = Validator::string()->minLength(0);
+        $this->assertTrue($validator->isValid(''));
+        $this->assertTrue($validator->isValid(' '));
+        $this->assertTrue($validator->isValid('č'));
+        $this->assertTrue($validator->isValid('0'));
+        $this->assertTrue($validator->isValid('lorem'));
+
+        $validator = Validator::string()->minLength(1);
+        $this->assertFalse($validator->isValid(''));
+        $this->assertTrue($validator->isValid(' '));
+        $this->assertTrue($validator->isValid('č'));
+        $this->assertTrue($validator->isValid('0'));
+        $this->assertTrue($validator->isValid('lorem'));
+
+
+        $validator = Validator::string()->minLength(5);
+        $this->assertFalse($validator->isValid(''));
+        $this->assertFalse($validator->isValid('0'));
+        $this->assertFalse($validator->isValid(' '));
+        $this->assertFalse($validator->isValid('č'));
+        $this->assertTrue($validator->isValid('lorem'));
+        $this->assertTrue($validator->isValid('lorem ipsum'));
+    }
+}
