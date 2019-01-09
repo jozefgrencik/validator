769c677c0bf7862a111916196675807f40e70fa1
Jozef Grencik <20k121@gmail.com>
2019-01-09 20:54:47 +0100

    add empty IntegerTest file

---
+++ tests/Validator/Rules/IntegerTest.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Validator\Rules;
+
+use PHPUnit\Framework\TestCase;
+
+class IntegerTest extends TestCase {
+
+    /**
+     * Tests for Validator::integer()->less()
+     */
+    public function testLess() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->ge()
+     */
+    public function testGe() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->le()
+     */
+    public function testLe() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->negative()
+     */
+    public function testNegative() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->eq()
+     */
+    public function testEq() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->lt()
+     */
+    public function testLt() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->equal()
+     */
+    public function testEqual() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->ne()
+     */
+    public function testNe() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->lessOrEqual()
+     */
+    public function testLessOrEqual() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->gt()
+     */
+    public function testGt() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->greater()
+     */
+    public function testGreater() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->greaterOrEqual()
+     */
+    public function testGreaterOrEqual() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->positive()
+     */
+    public function testPositive() {
+
+    }
+
+    /**
+     * Tests for Validator::integer()->notEqual()
+     */
+    public function testNotEqual() {
+
+    }
+}
